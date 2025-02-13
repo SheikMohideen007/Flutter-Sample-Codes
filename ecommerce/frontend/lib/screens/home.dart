60c24743f649ac4fe391a88585441cc715c9743f
SheikMohideen007 <sheikmohideen20010@gmail.com>
2025-02-13 20:02:59 +0530

    done

--- ecommerce/frontend/lib/screens/home.dart
+++ ecommerce/frontend/lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -51,9 +52,97 @@ class _HomeState extends State<Home> {
                 'Shop by Category',
                 style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
               ),
+              SizedBox(
+                height: devHeight * 0.15,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: devHeight * 0.02),
+                  child: ListView(scrollDirection: Axis.horizontal, children: [
+                    categoryCard(imgPath: 'p_img2.png', category: 'Men'),
+                    categoryCard(imgPath: 'p_img34.png', category: 'Women'),
+                    categoryCard(imgPath: 'p_img3.png', category: 'Kids'),
+                    categoryCard(imgPath: 'p_img2.png', category: 'Men'),
+                    categoryCard(imgPath: 'p_img34.png', category: 'Women'),
+                    categoryCard(imgPath: 'p_img3.png', category: 'Kids'),
+                  ]),
+                ),
+              ),
+              Text(
+                'Curated For You',
+                style: GoogleFonts.aDLaMDisplay(
+                    fontWeight: FontWeight.w300, fontSize: 22),
+              ),
+              Container(
+                color: Colors.grey,
+                height: 350,
+                width: 200,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: devHeight * 0.22,
+                      width: devWidth * 0.42,
+                      decoration: BoxDecoration(
+                          color: Colors.grey,
+                          image: DecorationImage(
+                              fit: BoxFit.fill,
+                              image: AssetImage('images/p_img1.png'))),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: devHeight * 0.01),
+                      child: Row(
+                        children: [
+                          Text(
+                            'H & M',
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black),
+                          ),
+                          SizedBox(width: devWidth * 0.03),
+                          SizedBox(
+                              width: 15,
+                              child: Image.asset('images/star_icon.png')),
+                          SizedBox(width: devWidth * 0.02),
+                          Text(
+                            '4.9 (124)',
+                            style: TextStyle(color: Colors.grey.shade700),
+                          )
+                        ],
+                      ),
+                    ),
+                    Text(
+                      'Oversized fit printed jbsjb',
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          overflow: TextOverflow.ellipsis),
+                    )
+                  ],
+                ),
+              )
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget categoryCard({required String imgPath, required String category}) {
+    return Padding(
+      padding: EdgeInsets.only(right: devWidth * 0.04),
+      child: Column(
+        children: [
+          Container(
+            height: devHeight * 0.08,
+            width: devWidth * 0.17,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.transparent,
+                image: DecorationImage(
+                    fit: BoxFit.cover, image: AssetImage('images/$imgPath'))),
+          ),
+          Text(category, style: TextStyle(fontWeight: FontWeight.bold))
+        ],
       ),
     );
   }
