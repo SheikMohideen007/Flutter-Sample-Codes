dc149aaa3415c1bdc9cdbdcc25a774b877fd4c74
SheikMohideen007 <sheikmohideen20010@gmail.com>
2025-02-26 19:41:25 +0530

    done

--- ecommerce/frontend/lib/screens/explore.dart
+++ ecommerce/frontend/lib/screens/explore.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/utils/product_card.dart';
 
 class Explore extends StatefulWidget {
   const Explore({super.key});
@@ -8,11 +9,112 @@ class Explore extends StatefulWidget {
 }
 
 class _ExploreState extends State<Explore> {
+  double devHeight = 0.0, devWidth = 0.0;
+  String symbol = "\$";
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Explore Menu'),
+    devHeight = MediaQuery.of(context).size.height;
+    devWidth = MediaQuery.of(context).size.width;
+    return SafeArea(
+      child: Scaffold(
+          body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            TextField(
+                style: TextStyle(color: Colors.black),
+                decoration: InputDecoration(
+                    hintText: 'Search',
+                    suffixIcon: Icon(Icons.search),
+                    contentPadding: EdgeInsets.all(5),
+                    isDense: true,
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey)),
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey)))),
+            SizedBox(height: 30),
+            SizedBox(
+              height: devHeight * 0.15,
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: devHeight * 0.02),
+                child: ListView(scrollDirection: Axis.horizontal, children: [
+                  categoryCard(imgPath: 'p_img2.png', category: 'Men'),
+                  categoryCard(imgPath: 'p_img34.png', category: 'Women'),
+                  categoryCard(imgPath: 'p_img3.png', category: 'Kids'),
+                  categoryCard(imgPath: 'p_img2.png', category: 'Men'),
+                  categoryCard(imgPath: 'p_img34.png', category: 'Women'),
+                  categoryCard(imgPath: 'p_img3.png', category: 'Kids'),
+                ]),
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ProductCard(
+                        devHeight: devHeight,
+                        devWidth: devWidth,
+                        symbol: symbol,
+                        cardWidth: devWidth * 0.45,
+                        cardHeight: devHeight * 0.4,
+                      ),
+                      ProductCard(
+                        devHeight: devHeight,
+                        devWidth: devWidth,
+                        symbol: symbol,
+                        cardWidth: devWidth * 0.45,
+                        cardHeight: devHeight * 0.4,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: devHeight * 0.02),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ProductCard(
+                        devHeight: devHeight,
+                        devWidth: devWidth,
+                        symbol: symbol,
+                        cardWidth: devWidth * 0.45,
+                        cardHeight: devHeight * 0.4,
+                      ),
+                      ProductCard(
+                        devHeight: devHeight,
+                        devWidth: devWidth,
+                        symbol: symbol,
+                        cardWidth: devWidth * 0.45,
+                        cardHeight: devHeight * 0.4,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      )),
+    );
+  }
+
+  Widget categoryCard({required String imgPath, required String category}) {
+    return Padding(
+      padding: EdgeInsets.only(right: devWidth * 0.04),
+      child: Column(
+        children: [
+          Container(
+            height: devHeight * 0.08,
+            width: devWidth * 0.17,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.transparent,
+                image: DecorationImage(
+                    fit: BoxFit.cover, image: AssetImage('images/$imgPath'))),
+          ),
+          Text(category, style: TextStyle(fontWeight: FontWeight.bold))
+        ],
       ),
     );
   }
--- ecommerce/frontend/lib/screens/profile.dart
+++ ecommerce/frontend/lib/screens/profile.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+// import 'package:image';
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -8,11 +12,70 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  final ImagePicker picker = ImagePicker();
+  String? image;
+
+  getFromGallery() async {
+    final XFile? file = await picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      image = file!.path;
+    });
+  }
+
+  getFromCamera() async {
+    final XFile? file = await picker.pickImage(source: ImageSource.camera);
+    setState(() {
+      image = file!.path;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Profile'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              child: image == null
+                  ? Image.asset('images/profile_icon.png')
+                  : Image.file(File(image!)),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+                onPressed: () {
+                  showModalBottomSheet(
+                      context: context,
+                      builder: (context) {
+                        return SizedBox(
+                          height: 120,
+                          child: Column(
+                            children: [
+                              ListTile(
+                                onTap: () {
+                                  getFromGallery();
+                                  Navigator.pop(context);
+                                },
+                                title: Text('Select from gallery'),
+                                trailing: Icon(Icons.photo),
+                              ),
+                              ListTile(
+                                onTap: () {
+                                  getFromCamera();
+                                  Navigator.pop(context);
+                                },
+                                title: Text('Select from camera'),
+                                trailing: Icon(Icons.camera),
+                              )
+                            ],
+                          ),
+                        );
+                      });
+                },
+                child: Text('Upload pic'))
+          ],
+        ),
       ),
     );
   }
